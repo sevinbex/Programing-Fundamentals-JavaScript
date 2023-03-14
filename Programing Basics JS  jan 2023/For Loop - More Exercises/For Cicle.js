9208c16b6d91819c5e5956195750eff34445b06d
sevinbex <sevinbex@abv.bg>
2023-03-15 01:08:53 +0200

    Added File to For Cycle

---
+++ Programing Basics JS  jan 2023/For Loop - More Exercises/For Cicle.js
@@ -0,0 +1,25 @@
+function nyc (input) {
+    let count = 10
+    //  console.log (count);
+    //  count--;
+    //  console.log (count);
+    //  count--;
+    //  console.log (count);
+    //  count--;
+    //  console.log (count);
+    //  count--;
+    //  console.log (count);
+    //  count--;
+    //  console.log (count);
+    //  count--;
+    //  console.log (count);
+    //  count--;
+    //  console.log (count);
+    //  count--;
+    //  console.log (count);
+    for (let count = 10; count >=1; count -= 1) {
+        
+    }
+}
+
+nyc()
