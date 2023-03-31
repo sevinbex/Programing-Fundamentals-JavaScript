8340706ac9137780a40d06dedf4ebe561dc09f6f
sevinbex <sevinbex@abv.bg>
2023-04-01 00:51:53 +0300

    arrays advance exercises complete

--- Programing Funds JS  May 2023/Arrays Advanced Exercise/6.bombNumbers.js
+++ Programing Funds JS  May 2023/Arrays Advanced Exercise/6.bombNumbers.js
@@ -1,18 +1,17 @@
 function bombNumbers(array, power) {
-  let position = power[0];
+  let bomb = power[0];
   let detonation = power[1];
-  let testArray = array.slice(0);
-
-  for (let i = 0; i < array.length; i++) {
-    if (array[i] === position) {
-      let spliced = array.splice(i - detonation, i + detonation);
-      //   console.log(spliced);
-    }
+  let indexOfBomb = array.indexOf(bomb);
+  // console.log(indexOfBomb);
+  while (indexOfBomb !== -1) {
+    let startExplosionIndex = Math.max(0, indexOfBomb - detonation);
+    // console.log(startExplosionIndex);
+    let explosionLength = detonation * 2 + 1;
+    // console.log(explosionLength);
+    array.splice(startExplosionIndex, explosionLength);
+    indexOfBomb = array.indexOf(bomb);
   }
-  let sum = 0;
-  for (num of array) {
-    sum += num;
-  }
+  let sum = array.reduce((a, b) => a + b, 0);
   console.log(sum);
 }
 
