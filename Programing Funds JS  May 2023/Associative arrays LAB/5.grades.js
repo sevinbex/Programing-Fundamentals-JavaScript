8e362ffa42efc7b7960911f67f079145c6750d3a
sevinbex <sevinbex@abv.bg>
2023-04-20 22:48:39 +0300

    last 2 exercises from LAB added

--- Programing Funds JS  May 2023/Associative arrays LAB/5.grades.js
+++ Programing Funds JS  May 2023/Associative arrays LAB/5.grades.js
@@ -2,14 +2,30 @@ function gradess(list) {
   let studList = {};
 
   for (const line of list) {
-    let [name, ...grades] = line.split(" ");
-    let sum = 0;
-    for (let i = 0; i < grades.length; i++) {
-      sum += Number(grades[i]);
+    let studentsListArr = line.split(" ");
+    let name = studentsListArr.shift();
+    let grades = studentsListArr.map((x) => Number(x));
+
+    if (!studList[name]) {
+      studList[name] = [];
     }
+
+    studList[name] = studList[name].concat(grades);
+  }
+
+  let studentsEntries = Object.entries(studList);
+  let sortedStudents = studentsEntries.sort((a, b) => a[0].localeCompare(b[0]));
+
+  for (const [name, grades] of sortedStudents) {
+    let sum = 0;
+
+    for (const grade of grades) {
+      sum += grade;
+    }
+
     let average = sum / grades.length;
-    // console.log(average.toFixed(2));
+
+    console.log(`${name}: ${average.toFixed(2)}`);
   }
 }
-
 gradess(["Lilly 4 6 6 5", "Tim 5 6", "Tammy 2 4 3", "Tim 6 6"]);
